0a8bd5f511378a0672101400ce20569980f66969
HuntingGo <cysys@cysys-Lenovo.(none)>
2013-10-09 18:40:28 +0800

    hehe

--- oj/usermail/usermail.py
+++ oj/usermail/usermail.py
@@ -9,12 +9,12 @@ import sys, os
 
 
 def mail_sc(req, fun, context):
-'''	
+	'''	
 	if not 'ojlogin' in context:
 		pageInfo = 'you must login first!'
 		title = '404 not found'
 		return render_to_response('error.html', {'pageInfo':pageInfo, 'title':title, 'context':context})
-'''	
+	'''	
 	if fun <= 0 or fun > 3:
 		pageInfo = 'the page not found!'
 		title = '404 not found!'
@@ -22,8 +22,7 @@ def mail_sc(req, fun, context):
 	username = context['ojlogin'].nick
 	if fun == 1:
 
-#	mails = Mail.objects.extra(where=["mail_to=usrname", "is_new=False"])
-	mails = Mail.objects.filter(mail_to=username)
+		mails = Mail.objects.filter(mail_to=username)
 	if len(mails) > 50:
 		mails = mails[0:50]
 	return render_to_response('mail.html', {'mails':mails, 'context':context})
