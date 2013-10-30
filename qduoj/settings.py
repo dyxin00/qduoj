92437e2bf0de92b6ca1733e5bc8ebb0bcb4f900c
dyxin00 <dyxin00@gmail.com>
2013-10-30 14:10:22 +0800

    jiong

--- qduoj/settings.py
+++ qduoj/settings.py
@@ -133,7 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-	'south',
+	#'south',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
