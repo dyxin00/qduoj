fb67bde40a80385d8c0a5f8ade6736a804299b29
dyxin00 <dyxin00@gmail.com>
2013-10-31 08:00:00 +0800

    jiong

--- qduoj/settings.py
+++ qduoj/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'qduoj',                      # Or path to database file if using sqlite3.
+        'NAME': 'qdu',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'root',
