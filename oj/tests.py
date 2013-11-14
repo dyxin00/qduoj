19ba0f2cdd40f7185931411e392016acee9530d1
dyxin00 <dyxin00@gmail.com>
2013-11-14 10:27:33 +0800

    dan yuan ce shi

--- oj/tests.py
+++ oj/tests.py
@@ -5,12 +5,18 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
-
+class test(TestCase):
+    def test (self):
+        c = Client()
+        respone = c.get('/')
+        self.assertEqual(respone.status_code,200)
+'''
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+'''
