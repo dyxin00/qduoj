7a6e19fc306761f051eeb2b8e091a854bc17243c
HuntingGo <hunting@hunting-Lenovo.(none)>
2013-12-02 16:27:26 +0800

    show the problem private

--- admin/adminproblem/adminproblem.py
+++ admin/adminproblem/adminproblem.py
@@ -2,35 +2,34 @@
 
 from django.shortcuts import render_to_response
 import os, os.path
-from oj.models import *
-from admin.is_login.is_login import *
-from oj.qduoj_config.qduoj_config import *
-from admin.uploadfile.uploadfile import *
+from oj.models import Problem 
+from oj.qduoj_config.qduoj_config import ADMIN_PAGE_PROBLEM_NUM
 from oj.util.util import paging
+from admin.uploadfile.uploadfile import *
 from admin.admin_backends import permission_asked
 from admin.forms import ProblemSearch, Admin_UploadFiles
 from admin.forms import ProblemForm
+from oj.tools import error
 
 @permission_asked('problem_add')
 def admin_problem_list_sc(req, context, page):
     page = int(page)
-    problem = Problem.objects.order_by('problem_id')
+    if ('minisiter' in context['ojlogin'].get_all_permission
+        or context['ojlogin'].isManager == True):
+        problem =  Problem.objects.order_by('problem_id')
+    else:    
+        problem = Problem.objects.filter(provider_id=context['ojlogin'].user_id).order_by('problem_id')
     (problemset, list_info) = paging(problem, ADMIN_PAGE_PROBLEM_NUM, page)
     permlist = context['ojlogin'].get_all_permission
     #if the requst is out of bound, error!
     if problemset == None: 
-        pageInfo = "page not found!!"
-        title = "404 not found"
-        return render_to_response('error.html', {
-            "pageInfo": pageInfo, 
-            "title":title, 
-            "context":context}
-        )
+        return error('404', 'page not found', context, 'admin_error.html');
     if req.method == 'POST':
         form = ProblemSearch(req.POST)
         if form.is_valid():
             proid = form.cleaned_data['search_id']
             problem = Problem.objects.filter(problem_id=proid)
+            problem = problem.filter(provider_id=context['ojlogin'].user_id)
             return render_to_response('admin_problem_list.html', {
                 'problems':problem, 'permlist':permlist, 'context':context}
             )
@@ -66,11 +65,7 @@ def admin_add_problem_sc(req, context):
 def admin_edit_problem_sc(req, context, proid):
     problem = Problem.objects.filter(problem_id=proid)
     if len(problem) == 0:
-        pageInfo = "page not found!"
-        return render_to_response('error.html', {
-            "pageInfo": pageInfo, 
-            "context":context}
-        )
+        return error('404', 'page not found', context, 'admin_error.html')
     if req.method == 'POST':
         form = ProblemForm(req.POST, instance = problem[0])
         if form.is_valid():
@@ -90,12 +85,7 @@ def problem_shift_mode_sc(req, context, proid, page, fun):
     problem = Problem.objects.filter(problem_id=proid)
     permlist = context['ojlogin'].get_all_permission
     if len(problem) == 0:
-        pageInfo = "no this problem"
-        return render_to_response('error.html', {
-            'context':context,
-            'pageInfo':pageInfo}
-        )
-        
+        return error('404', 'no this problem', context, 'admin_error.html')
     if fun == 'visible':
         if problem[0].visible == True:
             problem.update(visible=False)
@@ -178,7 +168,7 @@ def delete_testdata_sc(req, context, proid, filename):
         '/admin/problem_testdata/proid=%s'%proid
         )
 
-@permission_asked('minister')
+@permission_asked('problem_add')
 def download_testfile_sc(req, context, proid, filename):
     (targetFile, is_file) = is_file_exists(req, proid, filename)
     if is_file == 0 or is_file == 1:
